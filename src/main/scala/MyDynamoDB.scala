4576268a66c8b8bba969670cbabf0d41463c95c5
Kevin Duraj <kevin.x.duraj.-nd@disney.com>
2017-08-29 14:28:36 -0700

    2017-08-29 14:28

--- src/main/scala/MyDynamoDB.scala
+++ src/main/scala/MyDynamoDB.scala
@@ -5,6 +5,7 @@ import org.apache.hadoop.dynamodb.read.DynamoDBInputFormat
 import org.apache.hadoop.dynamodb.write.DynamoDBOutputFormat
 import org.apache.hadoop.mapred.JobConf
 import org.apache.hadoop.io.LongWritable
+import org.apache.spark.sql.SparkSession
 
 
 class MyDynamoDB(IP: String) {
@@ -12,14 +13,25 @@ class MyDynamoDB(IP: String) {
 
   def initialize_dynamodb(): Unit = {
 
-    var jobConf = new JobConf(sc.hadoopConfiguration)
+    val spark = SparkSession.builder
+        .master("local")
+        .appName("spark session dynamodb")
+        .getOrCreate()
+
+//    val df = spark.read.option("header","true").csv("src/main/resources/test.csv")
+//    df.show()
+
+
+    val jobConf = new JobConf(spark.sparkContext.hadoopConfiguration)
     jobConf.set("dynamodb.input.tableName", "test")
     jobConf.set("dynamodb.output.tableName", "test")
 
     jobConf.set("mapred.output.format.class", "org.apache.hadoop.dynamodb.write.DynamoDBOutputFormat")
     jobConf.set("mapred.input.format.class", "org.apache.hadoop.dynamodb.read.DynamoDBInputFormat")
 
-    var names = sc.hadoopRDD(jobConf, classOf[DynamoDBInputFormat], classOf[Text], classOf[DynamoDBItemWritable])
+    var names = spark.sparkContext.hadoopRDD(jobConf, classOf[DynamoDBInputFormat]
+                                              , classOf[Text]
+                                              , classOf[DynamoDBItemWritable])
     names.count()
 
   }
